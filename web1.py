d5aba6b6c86504b9a6d8357dae7c3bb49f0d87e8
rendyelang <lsmnrndy123@gmail.com>
2025-06-21 03:13:38 +0700

    fix: update app.run to bind to all interfaces

--- web1.py
+++ web1.py
@@ -46,4 +46,4 @@ def hello_world():
     return render_template('index.html', rekomendasi=rekomendasi)
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(host='0.0.0.0', debug=True)
